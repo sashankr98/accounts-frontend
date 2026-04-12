456e73a0c9243e95b4ae47c9b1a1b48544446e65
Sashank Ramachandran <sashank.r98@gmail.com>
2026-04-11 23:40:59 -0400

    Clean start for rewrite

--- src/utils/Utils.ts
+++
@@ -1,5 +0,0 @@
-export const dollarAmountFormatter = (value: number): string => {
-    const [start, end] = `${value}`.split(".") || [];
-    const v = `${start}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    return `${end ? `${v}.${end}` : `${v}`}`;
-}
